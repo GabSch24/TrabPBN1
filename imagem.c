7442a4bffcd37080a3f06ab6560c47d6e5577451
GabSch24 <144056706+GabSch24@users.noreply.github.com>
2024-04-09 11:17:53 -0300

    Perigoso Código

--- imagem.c
+++ imagem.c
@@ -1,28 +1,62 @@
 #include <stdio.h>
-typedef struct pixel
-{
-    int r;
-    int g;
-    int b;
-} pixel;
+#include <stdlib.h>
+enum Format{
+    RGB,
+    GREYSCALE
+};
 
+typedef struct
+{
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+} Pixel;
+
+typedef struct
+{
+    int height;
+    int width;
+    int max_value;
+    enum Format format;
+    Pixel *pixeis;
+} Imagem;
+
+Imagem readImage(FILE *fp){
+
+    Imagem img;
+
+    char formato[3];
+    fscanf(fp, "%s", formato);
+    //printf("%s\n", formato);
+    img.format = (formato[1] == '3') ? RGB : GREYSCALE;
+
+    int coluna,linha;
+    fscanf(fp, "%d%d", &coluna, &linha);
+    //printf("%d %d\n", coluna,linha);
+    img.height = linha;
+    img.width = coluna;
+
+    int valor_max;
+    fscanf(fp, "%d", &valor_max);
+    //printf("%d\n", valor_max);
+    img.max_value = valor_max;
+    Pixel *matriz;
+    matriz = (Pixel *)malloc((linha * coluna) * sizeof(Pixel));
+    for (int i = 0; i < linha*coluna; i++)
+    {
+        fscanf(fp,"%d %d %d", &matriz[i].r, &matriz[i].g, &matriz[i].b);
+    }
+    img.pixeis = matriz;
+    
+}
 void main(void){
     FILE *fp;
     fp = fopen("Bugio8.ppm","r");
 
-    char formato[3];
-    fscanf(fp, "%s", formato);
-    printf("%s\n", formato);
+    Imagem img = readImage(fp);
 
-    int coluna,linha;
-    fscanf(fp, "%d%d", &coluna, &linha);
-    printf("%d %d\n", coluna,linha);
-
-    int valor_max;
-    fscanf(fp, "%d", &valor_max);
-    printf("%d\n", valor_max);
-
-    pixel Pixeis[coluna*linha];
+    int coluna, linha;
+    Pixel Pixeis[coluna*linha];
 
     int i,j,count = 0;
     for(j=0; j<linha; j++){
@@ -33,14 +67,21 @@ void main(void){
             count++;
         }
     }
+    for (int i = 0; i < img.height * img.width; i++)
+    {
+        free(&img.pixeis[i]);
+    }
+    
     fclose(fp); 
 
     FILE *new_fp;
     new_fp = fopen("new_Bugio8.ppm","w");
     fprintf(new_fp, "P2\n");
     fprintf(new_fp, "%d %d\n", coluna, linha);
-    fprintf(new_fp, "%d\n", valor_max);
-    count = 0;
+    //fprintf(new_fp, "%d\n", valor_max);
+    
+    
+
     for(j=0; j<linha; j++){
         for (i = 0; i < coluna; i++)
         {
